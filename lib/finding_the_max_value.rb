7df645fbd295123bb34f18e5456c22fe1e127b3a
Joshua Morales <joshuapmorales@gmail.com>
2020-12-27 00:07:26 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -3,9 +3,12 @@ def find_max_value(scale)
   count = 0 
   
   max_value = -1
+  
   while scale[count] < scale[count].length do
-  if max_value < scale[count]
-  max_value = scale[count]
+    if max_value < scale[count]
+    max_value = scale[count]
+  end
+  
   count += l 
 end
 
