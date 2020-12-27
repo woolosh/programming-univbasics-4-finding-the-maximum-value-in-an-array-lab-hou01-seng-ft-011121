e39444a2bf2b61a9018c875f912f08da55fd7ec9
Joshua Morales <joshuapmorales@gmail.com>
2020-12-27 00:41:02 +0000

    Done.

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,3 +1,20 @@
-def find_max_value(array)
-  # Add your solution here
+require 'pry'
+
+def find_max_value(scale)
+
+  count = 0 
+  
+  max_value = -1
+
+  while count < scale.length do
+    if max_value < scale[count]
+    max_value = scale[count]
+  end
+  
+  count += 1 
+    
+  end
+  
+  return max_value
+
 end
